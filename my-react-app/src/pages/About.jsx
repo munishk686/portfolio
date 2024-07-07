d013c08cb04f3d6325c9d23e9e38d97af4da2aee
Munish <munishk686@gmail.com>
2024-07-07 22:39:03 +1200

    Updated the About page

--- my-react-app/src/pages/About.jsx
+++ my-react-app/src/pages/About.jsx
@@ -2,11 +2,22 @@ import React from "react";
 
 function About() {
     return (
-        <div>
-            <h1>About Me</h1>
-            <p>
-                I am a software developer with a passion for learning and teaching. I have experience working with a variety of technologies and programming languages. I am always looking for new challenges and opportunities to grow as a developer.
-            </p>
+        <div className="container">
+            <h1 className="about-h1">About Me</h1>
+            <h4>
+                Kia ora! I'm Munish, a dedicated and passionate Software Development student at AUT, nearing the completion of my Bachelor of Computer and Information Science. With my core and major papers finished, I'm currently focusing on my elective courses.
+
+                <br />I bring a unique blend of experience to the table, having worked as a bus driver for 21 years. This role has developed my skills in reliability, time management, and customer service. Now, I'm excited to switch careers and dive into the world of software development.
+
+                <br />In my spare time, I enjoy learning new programming languages, building projects, and exploring innovative tech solutions. I'm eager to contribute my skills and experience to a dynamic team in the tech industry.
+
+                <br />Feel free to explore my portfolio and see the projects I've been working on. I'm always open to new opportunities and connections.
+                <br /> <br />
+                Ngā mihi, <br />
+                Munish
+
+
+            </h4>
         </div>
     );
 }

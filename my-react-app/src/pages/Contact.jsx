cb468e13eca3ac75b5c2b38bdb0ab52d7b3e269b
munishk686 <103566689+munishk686@users.noreply.github.com>
2025-04-03 11:12:13 +1300

    Update Contact.jsx
    Updated Linkedin Account

--- my-react-app/src/pages/Contact.jsx
+++ my-react-app/src/pages/Contact.jsx
@@ -13,9 +13,9 @@ function Contact() {
 
             <h2 className="follow-title">Follow Me</h2>
             <ul className="social-links">
-                <li><a href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                <li><a href="https://www.linkedin.com/in/munishk686" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
                 <li><a href="https://github.com/munishk686/" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                <li><a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+                <li><a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">Twitter</a></li>
             </ul>
         </div>
     );

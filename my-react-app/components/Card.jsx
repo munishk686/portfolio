6558b1478a2fc251026abc15fbba2ce572679eca
munishk686 <munishk686@gmail.com>
2025-04-21 20:07:05 +1200

    {commit_message}

--- my-react-app/components/Card.jsx
+++ my-react-app/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
         <div className="col-md-4">
             <div className="card">
                 <div className="card-body">
-                    <h5 className="card-title">{props.title}</h5>
+                   <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">{props.description}</p>
                     <p><a href={props.link} target="_blank" rel="noopener noreferrer">Link</a></p>
                     <p><a href={props.git} target="_blank" rel="noopener noreferrer">GitHub</a></p>

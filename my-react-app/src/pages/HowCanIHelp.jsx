9ccac8ca67694183ce2488accb3a4a2ea305ebaf
munishk686 <munishk686@gmail.com>
2025-06-30 12:25:06 +1200

    Updated email section

--- my-react-app/src/pages/HowCanIHelp.jsx
+++ my-react-app/src/pages/HowCanIHelp.jsx
@@ -1,8 +1,23 @@
+import {useRef} from 'react';
+import emailjs from 'emailjs-com';
 function HowCanIHelp() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_69m147t', 'template_qcawhyk', form.current, 'dv0ehT6Di1pRtXn-w')
+            .then(() => {
+                alert("Message sent successfully!");
+            }, (error) => {
+                alert("Failed to send message. Please try again later.", error.text);
+            });
+            e.target.reset();
+    };
   return (
     <div>
         <h5 className="text-start font-colour mx-2">How Can I Help You?</h5>
-        <form className="my-4 mx-2">
+        <form ref={form} onSubmit={sendEmail} className="my-4 mx-2">
             <div className="mb-3 text-start">
                 <label htmlFor="name" className="form-label">Full Name</label>
                 <input type="text" className="form-control" id="name"  required />
@@ -12,8 +27,8 @@ function HowCanIHelp() {
                 <input type="email" className="form-control" id="email"  required />
             </div>
             <div className="mb-3 text-start">
-                <label htmlFor="email" className="form-label">Subject</label>
-                <input type="email" className="form-control" id="email"  required />
+                <label htmlFor="subject" className="form-label">Subject</label>
+                <input type="subject" className="form-control" id="subject"  required />
             </div>
             <div className="mb-3 text-start">
                 <label htmlFor="message" className="form-label">Message</label>

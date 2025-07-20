b16456fca0c972fd78822f85db5020250ac63dfd
munishk686 <munishk686@gmail.com>
2025-07-20 20:02:04 +1200

    Updated body background colour

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     return (
         <div>
             <section id="home">
-                <div className="row align-items-center secound">
+                <div className="row align-items-center secound header">
                     <div className="col-md-4">
                         <img
                             src="/images/photo.jpg"
@@ -41,6 +41,7 @@ function Home() {
                                 alt="Download Arrow"
                                 width="14"
                                 height="14"
+                                style={{ marginRight: "5px" }} 
                             />
                             Download CV
                         </a>
@@ -53,7 +54,7 @@ function Home() {
                         className="svg-wave"
                     >
                         <path
-                            fill="#ced1ba"
+                            fill="#D7D7D7"
                             fillOpacity="1"
                             d="M0,288L60,266.7C120,245,240,203,360,181.3C480,160,600,160,720,186.7C840,213,960,267,1080,266.7C1200,267,1320,213,1380,186.7L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
                         ></path>
